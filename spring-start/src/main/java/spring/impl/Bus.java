aa24ff937e2fc4be68974ec0f9292509e6b2d1a8
Marcin Dzierzak <marcin.dzierzak@realis.pro>
2023-10-16 12:26:57 +0000

    context - @PostConstruct

--- spring-start/src/main/java/spring/impl/Bus.java
+++ spring-start/src/main/java/spring/impl/Bus.java
@@ -1,11 +1,18 @@
 package spring.impl;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 import spring.Person;
 import spring.Transportation;
 
 @Component("bus")
 public class Bus implements Transportation {
+
+    @PostConstruct
+    void init(){
+        System.out.println("preparing bus for a journey");
+    }
+
     @Override
     public void transport(Person p) {
         System.out.println("person " + p + " is being transported by BUS");
